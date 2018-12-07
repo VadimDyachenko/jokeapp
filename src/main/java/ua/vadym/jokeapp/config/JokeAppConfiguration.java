104f5ceffd4ab208f3b0a7c572f2d54d47723d84
Vadym <vadim.dyachenko@gmail.com>
2018-12-07 08:36:12 +0200

    Added Java Spring configuration class

---
+++ src/main/java/ua/vadym/jokeapp/config/JokeAppConfiguration.java
@@ -0,0 +1,14 @@
+package ua.vadym.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeAppConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
